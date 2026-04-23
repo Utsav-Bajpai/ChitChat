4b37e6eadd62131a0a11b224eb496240870949e1
Utsav-Bajpai <utsavb736@gmail.com>
2026-04-23 21:41:16 +0530

    change

--- server.js
+++ server.js
@@ -254,5 +254,5 @@ io.on("connection", (socket) => {
 // ─────────────────────────────────────────────
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
